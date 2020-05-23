c88f1dbbb3abc2e553889e693b99907d230eb02d
Rob Rokosz <robert.rokosz@mobilise.cloud>
2020-05-23 16:44:47 +0100

    Service set up.

---
+++ user_service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(debug=True)
